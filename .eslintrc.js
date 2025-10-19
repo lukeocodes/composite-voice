c3d20332fa5e16c7974ad7b545172f6d4da56301
Luke Oliff <luke@lukeoliff.com>
2025-10-19 10:32:48 +0100

    chore: add project configuration files
    - Add ESLint configuration for code quality
    - Add Prettier configuration for code formatting
    - Add TypeScript configuration for compilation
    - Add Jest configuration for testing
    - Add .gitignore for version control

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/require-await': 'off',
+  },
+  ignorePatterns: ['dist/', 'node_modules/', '*.js'],
+};
